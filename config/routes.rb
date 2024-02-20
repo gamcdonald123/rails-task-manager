a592cdac5148ccb22f459bdb765ae6bd7e2d92f6
Guy McDonald <guy@goodfishgroup.com>
2024-02-20 13:57:03 +0000

    Completed core functionality. TODO: formatting.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "tasks", to: "tasks#index"
+
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  get "new", to: "tasks#new", as: "new"
+
+  post "tasks", to: "tasks#create", as: "create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+
+  patch "tasks/:id", to: "tasks#update", as: "update"
+
+  delete "tasks/:id", to: "tasks#delete", as: "delete"
+
 end
